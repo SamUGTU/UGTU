ef6a1744ff48881272304666664038da23d71214
SamUGTU <belenkosemen@yandex.ru>
2017-04-22 11:59:23 +0300

    first

---
+++ app/page-started/page-started.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+     
+@Component({
+    moduleId: module.id,
+    selector: 'page-started',
+    templateUrl: 'page-started.component.html',
+    styleUrls: [ 'page-started.component.css', '../../css/fontsstyles.css']
+})
+export class PageStartedComponent { 
+
+}
